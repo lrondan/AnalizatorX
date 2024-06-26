e41643a28c0dc19e5fd3aa14e347a7cd81c515e6
´lrondan´ <lrondan1126@gmail.com>
2024-06-26 19:26:10 +0200

    tercer commit

--- csv_files/visor.py
+++ csv_files/visor.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import *
 from tkinter import filedialog
 from tkinter import messagebox as mBox
-import csv
+import matplotlib.pyplot as mpl
 import pandas as pd
 
 class Ventana2:
@@ -13,24 +13,34 @@ class Ventana2:
         self.roott.resizable(0,0)
         self.Labels()
         self.Botones()
+        self.Entradas()
 
 
 
     def Labels(self):
         Label(self.roott,text='Paso1. Elegir archivo con extension .csv').place(x=0, y=0)
         Label(self.roott,text='Paso2. Crear un DataFrame por columnas a partir de los datos').place(x=0, y=100)
-        Label(self.roott, text='Paso3. Procesar datos').place(x=0, y=160)
+        Label(self.roott, text='Paso3. Visualizar').place(x=0, y=160)
         Label(self.roott, text='Metodo').place(x=0, y=180)
-        Label(self.roott, text='Grafico').place(x=200, y=180)
+        Label(self.roott, text='Grafico').place(x=150, y=180)
+        Label(self.roott, text='X=').place(x=210, y=200)
+        Label(self.roott, text='Y=').place(x=210, y=230)
+        
 
         
 
     def Botones(self):
         Button(self.roott, text='Buscar', command=self.Cargar).place(x=0, y=25)
         Button(self.roott,text='Crear', command=self.Crear).place(x=0, y=120)
-        Button(self.roott,text='Binning', command=self.Binns).place(x=0, y=200)
-        Button(self.roott,text='Plot Bins', command=self.Binns).place(x=200, y=200)
+        Button(self.roott,text='Agrupar', command=self.Binns).place(x=0, y=200)
+        Button(self.roott,text='Plot', command=self.PlotBinns).place(x=150, y=200)
+        Button(self.roott, text='Salir', command=self.Salir).place(x=300, y=305)
 
+    def Entradas(self):
+        self.col1 = IntVar()
+        self.col2 = IntVar()
+        Entry(self.roott, textvariable=self.col1, width=15).place(x=230, y=200)
+        Entry(self.roott, textvariable=self.col2, width=15).place(x=230, y=230)
 
     def Cargar(self):
         self.filename_csv = StringVar()
@@ -50,8 +60,21 @@ class Ventana2:
         raiz.mainloop()
 
     def Binns(self):
-        pass
+        self.columnas1 = self.col1.get()
+        self.columnas2 = self.col2.get()
+        self.ejex = self.df1.iloc[2:, self.columnas1]
+        self.ejey = self.df1.iloc[2:, self.columnas2]
+        mBox.showinfo('Nuevo Set', f'{self.ejex, self.ejey}')
+        print(self.ejex, self.ejey)
 
+    def PlotBinns(self):
+        self.ejex.astype('int64')
+        mpl.plot(self.ejex, self.ejey)
+        mpl.show()
+
+    def Salir(self):
+        self.roott.quit()
+        self.roott.destroy()
 
 objeto = Ventana2(Tk())
 objeto.roott.mainloop()
