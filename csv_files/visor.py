8f4a99dd707e6a1aa79d2be1dc4a9981c8d1033a
´lrondan´ <lrondan1126@gmail.com>
2024-06-26 17:14:15 +0200

    secound commit

--- csv_files/visor.py
+++ csv_files/visor.py
@@ -1,41 +1,57 @@
 import tkinter as tk
 from tkinter import *
 from tkinter import filedialog
+from tkinter import messagebox as mBox
 import csv
 import pandas as pd
 
-class Ventana:
+class Ventana2:
     def __init__(self, interfaz) -> None:
-        self.root = interfaz
-        self.root.title('csv files')
-        self.root.geometry('350x350')
-        self.root.resizable(0,0)
+        self.roott = interfaz
+        self.roott.title('csv files')
+        self.roott.geometry('350x350')
+        self.roott.resizable(0,0)
         self.Labels()
         self.Botones()
 
 
+
     def Labels(self):
-        Label(self.root,text='Paso1. Elegir archivo con extension .csv').place(x=0, y=0)
-        Label(self.root,text='Paso2. Crear un DataFrame a partir de los datos').place(x=0, y=0)
+        Label(self.roott,text='Paso1. Elegir archivo con extension .csv').place(x=0, y=0)
+        Label(self.roott,text='Paso2. Crear un DataFrame por columnas a partir de los datos').place(x=0, y=100)
+        Label(self.roott, text='Paso3. Procesar datos').place(x=0, y=160)
+        Label(self.roott, text='Metodo').place(x=0, y=180)
+        Label(self.roott, text='Grafico').place(x=200, y=180)
+
         
 
     def Botones(self):
-        Button(text='Buscar', command=self.Cargar).place(x=0, y=25)
+        Button(self.roott, text='Buscar', command=self.Cargar).place(x=0, y=25)
+        Button(self.roott,text='Crear', command=self.Crear).place(x=0, y=120)
+        Button(self.roott,text='Binning', command=self.Binns).place(x=0, y=200)
+        Button(self.roott,text='Plot Bins', command=self.Binns).place(x=200, y=200)
+
 
     def Cargar(self):
         self.filename_csv = StringVar()
         self.filename_csv = filedialog.askopenfilename(filetypes=('archivos .csv', '* .csv'))
-        Label(self.root, text=f' La direccion del archivo que se esta usando es:\n {self.filename_csv}', justify='left', state='disabled').place(x=0 ,y=50)
+        Label(self.roott, text=f' La direccion del archivo que se esta usando es:\n {self.filename_csv}', justify='left', state='disabled').place(x=0 ,y=50)
         with open(f'{self.filename_csv}', 'r')as file:
             self.path = file.read()
-            self.df = pd.read_csv(self.path)
-            print(self.df)
-        
-        
+            self.df = pd.read_csv(self.path)    
 
-        
-        
-        
+    def Crear(self):
+        self.url = self.filename_csv
+        self.df1 = pd.read_csv(self.url, header=None)
+        raiz = Tk()
+        raiz.title('DataFrame')
+        raiz.resizable(0,0)
+        Label(raiz, text=f'{self.df1}', justify='left').pack()
+        raiz.mainloop()
 
-objeto = Ventana(Tk())
-objeto.root.mainloop()
+    def Binns(self):
+        pass
+
+
+objeto = Ventana2(Tk())
+objeto.roott.mainloop()
