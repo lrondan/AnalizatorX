f43731ccf889797a640409325a5cb3c15292c090
´lrondan´ <lrondan1126@gmail.com>
2024-06-28 00:19:21 +0200

    fix menu

--- menu/contact.py
+++ menu/contact.py
@@ -1,4 +1,4 @@
 from tkinter import *
 from tkinter import messagebox as mBox1
 
-mBox1.showinfo('Contacto', 'Autor: Luis Alexis Rojas Rondán\nGitHub: lrondan\nSe puede encontar más información acerca del análisis de datos en los repositorios adjuntos.')
+mBox1.showinfo('Contacto', 'Autor: Luis Alexis Rojas Rondán\nGitHub: lrondan\ngmail: lrondan1126@gmail.com\nSe puede encontar más información acerca del análisis de datos en los repositorios adjuntos.')
